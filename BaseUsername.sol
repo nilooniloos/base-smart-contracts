6bb1ad94d53b51c7a4b6488f6f11132a608b8a74
Niloo <95609208+nilooniloos@users.noreply.github.com>
2026-09-07 21:37:43 +0200

    Create BaseUsername.sol

---
+++ BaseUsername.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract BaseUsername {
+    mapping(address => string) public username;
+    mapping(string => address) public ownerOf;
+
+    function setUsername(string calldata name) external {
+        require(bytes(name).length >= 3, "Name too short");
+        require(ownerOf[name] == address(0), "Name already taken");
+
+        username[msg.sender] = name;
+        ownerOf[name] = msg.sender;
+    }
+
+    function getUsername(address user) external view returns (string memory) {
+        return username[user];
+    }
+}
