bde3fb2f81659f59269afa25ced65a04847de460
Niloo <95609208+nilooniloos@users.noreply.github.com>
2026-09-19 21:58:13 +0200

    Create BaseSnapshot.sol

---
+++ BaseSnapshot.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract BaseSnapshot {
+    struct Checkpoint {
+        uint256 blockNumber;
+        uint256 value;
+    }
+
+    mapping(address => Checkpoint[]) private checkpoints;
+
+    function record(uint256 value) external {
+        checkpoints[msg.sender].push(
+            Checkpoint({
+                blockNumber: block.number,
+                value: value
+            })
+        );
+    }
+
+    function getCheckpointCount(address user)
+        external
+        view
+        returns (uint256)
+    {
+        return checkpoints[user].length;
+    }
+
+    function getCheckpoint(
+        address user,
+        uint256 index
+    )
+        external
+        view
+        returns (uint256 blockNumber, uint256 value)
+    {
+        Checkpoint memory item = checkpoints[user][index];
+        return (item.blockNumber, item.value);
+    }
+}
